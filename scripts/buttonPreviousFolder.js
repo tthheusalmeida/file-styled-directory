68307c749931a679a367f9286100190a040bdefc
Matheus Almeida <tthheusalmeida@gmail.com>
2023-02-10 14:40:40 -0300

    chore: remove toggle theme button

--- scripts/buttonPreviousFolder.js
+++ scripts/buttonPreviousFolder.js
@@ -1,9 +1,10 @@
 function changePreviousButtonContent () { 
   const previousButtonChild = document.getElementById('parentDirLink');
-
   previousButtonChild.classList.remove('icon');
   previousButtonChild.classList.remove('up');
   previousButtonChild.innerText = '◀️';
+
+  previousButtonChild.style.display = 'block';
 };
 
 const previousButtonChild = changePreviousButtonContent();
